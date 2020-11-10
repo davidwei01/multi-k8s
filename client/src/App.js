d370d8b696853561018e7151e6a6e662e6554972
Dave Wei <qwei@orbistechnologies.com>
2020-11-10 12:02:30 -0500

    changed to davidwei01docker

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+Fib Calculator davidwei01docker version
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t davidwei01/multi-client:latest -t davidwei01/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t davidwei01/multi-server:latest -t davidwei01/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t davidwei01/multi-worker:latest -t davidwei01/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t davidwei01docker/multi-client:latest -t davidwei01docker/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t davidwei01docker/multi-server:latest -t davidwei01docker/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t davidwei01docker/multi-worker:latest -t davidwei01docker/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push davidwei01/multi-client:latest
-docker push davidwei01/multi-server:latest
-docker push davidwei01/multi-worker:latest
+docker push davidwei01docker/multi-client:latest
+docker push davidwei01docker/multi-server:latest
+docker push davidwei01docker/multi-worker:latest
 
-docker push davidwei01/multi-client:$SHA
-docker push davidwei01/multi-server:$SHA
-docker push davidwei01/multi-worker:$SHA
+docker push davidwei01docker/multi-client:$SHA
+docker push davidwei01docker/multi-server:$SHA
+docker push davidwei01docker/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=davidwei01/multi-server:$SHA
-kubectl set image deployments/client-deployment client=davidwei01/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=davidwei01/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=davidwei01docker/multi-server:$SHA
+kubectl set image deployments/client-deployment client=davidwei01docker/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=davidwei01docker/multi-worker:$SHA
